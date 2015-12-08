7f3a1a74fd472dfcd4bdd2a2ca6d642bfc1b380c
Artem Tyurin <artem.tyurin@gmail.com>
2015-12-08 22:10:31 +0100

    Minor refactoring for puzzle 7

--- puzzle7/index.js
+++ puzzle7/index.js
@@ -3,63 +3,59 @@ var fs = require('fs');
 var input = fs.readFileSync('input.txt').toString().trim();
 
 var actions = {
-    'AND': (a, b) => a & b,
-    'OR': (a, b) => a | b,
+    'AND':    (a, b) => a & b,
+    'OR':     (a, b) => a | b,
     'LSHIFT': (a, b) => a << b,
     'RSHIFT': (a, b) => a >> b,
-    'NOT': (a) => ~a > 0 ? ~a : (65536 + ~a)
+
+    'NOT':    (a) => ~a > 0 ? ~a : (65536 + ~a)
 };
 
 var evaluated = {};
 
-function applyCommand(state, command) {
+function evaluate(state, s) {
+    var match = String(s).match(/\d+/);
 
-    function evaluate(s) {
-        var match = String(s).match(/\d+/);
+    if (match) {
+        return Number(s);
+    }
 
-        if (match) {
-            return Number(s);
-        }
-
-        // we get a loop without remembering the evaluation
-        if (evaluated[s]) {
-            return evaluated[s];
-        }
-
-        var result = state[s]();
-
-        evaluated[s] = result;
-
+    // we get a loop without remembering the evaluation
+    if (evaluated[s]) {
         return evaluated[s];
     }
 
+    var result = state[s]();
+
+    evaluated[s] = result;
+
+    return evaluated[s];
+}
+
+function applyCommand(state, command) {
     var output;
-
-    if (command.input.type === 'expression') {
-        output = function() {
-            return evaluate(command.input.value);
-        }
-    }
-
-    if (command.input.type === 'binary-operation') {
-        output = function() {
-            return actions[command.input.op](
-                evaluate(command.input.left),
-                evaluate(command.input.right)
-            );
-        }
-    }
-
-    if (command.input.type === 'unary-operation') {
-        output = function() {
-            return actions[command.input.op](
-                evaluate(command.input.value)
-            );
-        }
-    }
 
     if (state[command.output]) {
         throw new Error('Already wired!');
+    }
+
+    if (command.input.type === 'expression') {
+        output = () => evaluate(state, command.input.value);
+    }
+
+    if (command.input.type === 'binary-operation') {
+        output = () =>
+            actions[command.input.op](
+                evaluate(state, command.input.left),
+                evaluate(state, command.input.right)
+            );
+    }
+
+    if (command.input.type === 'unary-operation') {
+        output = () =>
+            actions[command.input.op](
+                evaluate(state, command.input.value)
+            );
     }
 
     state[command.output] = output;
